f17cfe5574efd6b3f692f071467b497e88c0a5cc
Levi Ackerman <pratapagni14@gmail.com>
2026-07-13 00:06:01 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,16 @@
+# \n - next line
+# \t - tab
+# \\ - backslash
+# \' - insert a single quote inside a single quoted string
+
+# \n
+print("hello world.\nhow are you?")
+
+# \t
+print("john\t20")
+
+# \\
+print("new\\old")
+
+# \'
+print('this is python\'s class')
